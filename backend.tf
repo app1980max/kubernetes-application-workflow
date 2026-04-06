27ae3b0b48158268c8bc1bdc65e8d1ea3eb7d015
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-07 00:52:40 +0300

    update

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
     bucket   = "vclusters"
     key      = "vcluster-dev2/terraform.tfstate"
     region   = "us-west-rack-2"
-    endpoint = "http://minio:9000"
+    endpoint = "https://s3-dev.appflex.io"
 
     skip_credentials_validation = true
     skip_metadata_api_check     = true
