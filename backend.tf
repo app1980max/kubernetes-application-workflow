e5f0379f314dd4c9e30cc36cff5ad1f682bca523
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-07 01:26:22 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,13 +5,15 @@ terraform {
     key    = "vcluster-dev2/terraform.tfstate"
     region = "us-west-rack-2"
 
-    endpoints = {
-      s3 = "https://s3-dev.appflex.io"
-    }
-
+    endpoint = "https://s3-dev.appflex.io"
+   
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
     use_path_style              = true
   }
 }
+
+
+
+endpoint = "http://office-minio.leadercapital.net:9000"
