087429fb204260ec796958b94187700b3ffbc4a5
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-07 01:20:18 +0300

    update

--- backend.tf
+++ backend.tf
@@ -5,25 +5,13 @@ terraform {
     key    = "vcluster-dev2/terraform.tfstate"
     region = "us-west-rack-2"
 
-    # Endpoints object (Terraform 1.10+)
     endpoints = {
       s3 = "https://s3-dev.appflex.io"
     }
 
-    # MinIO-specific flags
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    use_path_style              = true  # updated from deprecated force_path_style
+    use_path_style              = true
   }
 }
-
-# Optional AWS provider (only if you have AWS resources)
-provider "aws" {
-  region                     = "us-west-rack-2"
-
-  # Prevent Terraform from calling AWS STS/IAM for MinIO backend
-  skip_requesting_account_id  = true
-  skip_credentials_validation = true
-}
-
