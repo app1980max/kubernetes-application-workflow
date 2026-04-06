964e326e211f9ea6b202b5b0a30a688b4cd0e417
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-06 13:43:21 +0300

    update

--- modules/ingress/variables.tf
+++ modules/ingress/variables.tf
@@ -1,14 +1,27 @@
 
+variable "name" {
+  description = "The name of the service/ingress"
+  type        = string
+  default     = "ingress-route-app"
+}
+
+variable "namespace" {
+  description = "Namespace where the ingress and service are deployed"
+  type        = string
+  default     = "default"
+}
+
 variable "host" {
-  description = "Domain name for ingress (e.g. app.example.com)"
+  description = "Hostname for ingress"
   type        = string
   default     = "app-cluster2.appflex.io"
 }
 
-variable "ingress_class_name" {
-  description = "Ingress class (nginx or traefik)"
-  type        = string
-  default     = "nginx"
+variable "service_port" {
+  description = "Port of the backend service"
+  type        = number
+  default     = "80"
 }
+
 
 
