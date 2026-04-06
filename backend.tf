0a37315afa1ccbd3b14ef3125b7b4ba0c5183653
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-07 00:54:28 +0300

    update

--- backend.tf
+++ backend.tf
@@ -1,10 +1,13 @@
 
 terraform {
   backend "s3" {
-    bucket   = "vclusters"
-    key      = "vcluster-dev2/terraform.tfstate"
-    region   = "us-west-rack-2"
-    endpoint = "https://s3-dev.appflex.io"
+    bucket = "vclusters"
+    key    = "vcluster-dev2/terraform.tfstate"
+    region = "us-west-rack-2"
+
+    endpoints {
+      s3 = "https://s3-dev.appflex.io"
+    }
 
     skip_credentials_validation = true
     skip_metadata_api_check     = true
@@ -12,4 +15,3 @@ terraform {
     force_path_style            = true
   }
 }
-
