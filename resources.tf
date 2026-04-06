603378558c1b70480cae49d220822825ae12b666
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-06 15:18:18 +0300

    update

--- resources.tf
+++ resources.tf
@@ -6,7 +6,7 @@ module "httpd" {
 
   name   = "httpd-server"
   namespace = "default"
-  replicas  = 2
+  replicas  = 3
   image = "virtapp/apache:7f6c4bf4-3-6"
   service_port = 8080
   service_type = "ClusterIP"
