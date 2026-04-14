7bc19b5d4abcd721f7c1da11ff066ec04e89ab19
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-15 01:42:09 +0300

    update

--- backend.tf
+++ backend.tf
@@ -8,14 +8,13 @@ terraform {
       s3 = "https://s3-api.appflex.io"
     }
 
-    force_path_style = true
+    use_path_style = true
 
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
     skip_requesting_account_id  = true
 
-    # 🔥 IMPORTANT FOR MINIO + TERRAFORM
     skip_s3_checksum = true
   }
 }
